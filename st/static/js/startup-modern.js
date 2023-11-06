e01bd1642d12e6313384a53f0c77587c2c8c8276
SaiRameshkumar14 <saiddiwaker141102@gmail.com>
2023-11-06 20:38:03 +0530

    new

--- st/static/js/startup-modern.js
+++ st/static/js/startup-modern.js
@@ -1,12 +1,13 @@
+
 (function() {
-  "use strict"; // Start of use strict
+  "use strict";
 
   var mainNav = document.querySelector('#mainNav');
 
+
   if (mainNav) {
 
-    // Collapse Navbar
-    var collapseNavbar = function() {
+    var collapseNavbar = function() {d
 
       var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
@@ -16,11 +17,8 @@
         mainNav.classList.remove("navbar-shrink");
       }
     };
-    // Collapse now if page is not at top
     collapseNavbar();
-    // Collapse the navbar when page is scrolled
     document.addEventListener("scroll", collapseNavbar);
   }
 
-})(); // End of use strict
-
+})();
